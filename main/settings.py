47863455244fabccf853e4bfe3be30ff0cbb6107
Chika Dim <cckrist@outlook.com>
2023-11-28 17:07:46 +0000

    Add whitenoise to fix admin page

--- main/settings.py
+++ main/settings.py
@@ -101,6 +101,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 if 'CLIENT_ORIGIN' in os.environ:
