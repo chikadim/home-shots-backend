58617f699b76cf8dfaf980aa6f9527c9a118c4dd
Chika Dim <cckrist@outlook.com>
2023-12-05 13:12:58 +0000

    Add PostDetail tests

--- posts/tests.py
+++ posts/tests.py
@@ -23,6 +23,35 @@ class PostListViewTests(APITestCase):
         self.assertEqual(count, 1)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    def test_user_not_logged_in_cant_create_post(self):
-        response = self.client.post('/posts/', {'title': 'a title'})
+
+class PostDetailViewTests(APITestCase):
+    def setUp(self):
+        john = User.objects.create_user(username='john', password='pass')
+        janet = User.objects.create_user(username='janet', password='pass')
+        Post.objects.create(
+            owner=john, title='a title', content='john content'
+        )
+        Post.objects.create(
+            owner=janet, title='another title', content='janet content'
+        )
+
+    def test_can_retrieve_post_using_valid_id(self):
+        response = self.client.get('/posts/1/')
+        self.assertEqual(response.data['title'], 'a title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cant_retrieve_post_using_invalid_id(self):
+        response = self.client.get('/posts/999/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_update_own_post(self):
+        self.client.login(username='john', password='pass')
+        response = self.client.put('/posts/1/', {'title': 'a new title'})
+        post = Post.objects.filter(pk=1).first()
+        self.assertEqual(post.title, 'a new title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_user_cant_update_another_users_post(self):
+        self.client.login(username='john', password='pass')
+        response = self.client.put('/posts/2/', {'title': 'a new title'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
