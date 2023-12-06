f3c3dad30746704a7ca0215b6d34f7e0aa19466c
Chika Dim <cckrist@outlook.com>
2023-12-06 06:57:42 +0000

    Fix auth issues in settings.py

--- main/settings.py
+++ main/settings.py
@@ -99,7 +99,7 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
